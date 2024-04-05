472d7727e026d1f358a4a7d85bbcf8f7c512a409
JavierFdez23 <162705862+JavierFdez23@users.noreply.github.com>
2024-04-05 13:53:48 +0200

    Update ContarPares.java

--- Proyecto3/src/contarpares/ContarPares.java
+++ Proyecto3/src/contarpares/ContarPares.java
@@ -4,12 +4,12 @@ public class ContarPares {
 
 	public static void main(String[] args) {
 		int contadorPares = 0;
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 0; i <= 500; i++) {
 			if (i % 2 == 0)
 				contadorPares++;
 		}
 		//imprimir resultado para saber si es par o no
-		System.out.println("Hay " + contadorPares + " numeros pares del 0 al 10");
+		System.out.println("Hay " + contadorPares + " numeros pares del 0 al 50");
 	}
 
 }
